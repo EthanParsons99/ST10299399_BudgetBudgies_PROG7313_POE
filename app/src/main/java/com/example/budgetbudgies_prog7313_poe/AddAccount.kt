cd1ea2376c3effec2ad3d111a3c495e46b4677cd
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-04-25 09:31:04 +0200

    Navigation between accounts and add accounts page

--- app/src/main/java/com/example/budgetbudgies_prog7313_poe/AddAccount.kt
+++ app/src/main/java/com/example/budgetbudgies_prog7313_poe/AddAccount.kt
@@ -3,6 +3,7 @@ package com.example.budgetbudgies_prog7313_poe
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+//Add Account Activity
 class AddAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

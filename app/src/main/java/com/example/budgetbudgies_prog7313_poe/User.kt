713d0b59da719f7e593058d2735f2673531b31da
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-04-27 11:06:16 +0200

    Added user table

---
+++ app/src/main/java/com/example/budgetbudgies_prog7313_poe/User.kt
@@ -0,0 +1,13 @@
+package com.example.budgetbudgies_prog7313_poe
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val email: String,
+    val password: String,
+    val firstName: String,
+    val lastName: String
+)

8faf462f3c9f91d2f004c3b1dc1ccf00f6ef45c9
Calwyn Andrew Govender <130094274+ST10303017@users.noreply.github.com>
2025-04-28 04:50:24 +0200

    Added the UserDao

---
+++ app/src/main/java/com/example/budgetbudgies_prog7313_poe/UserDao.kt
@@ -0,0 +1,19 @@
+package com.example.budgetbudgies_prog7313_poe
+
+import androidx.room.OnConflictStrategy
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.budgetbudgies_prog7313_poe.User
+
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User): Long
+
+    @Query("SELECT * FROM user_table WHERE email = :email LIMIT 1")
+    suspend fun getUserByEmail(email: String): User?
+
+    @Query("SELECT * FROM user_table WHERE userid = :userId LIMIT 1")
+    suspend fun getUserById(userId: Int): User?
+}
